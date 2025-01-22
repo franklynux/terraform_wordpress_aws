0991ed4d95a39c1337e56159028cd47fa4cdc8de
franklynux <chiwando3@utlook.com>
2025-01-22 12:35:25 +0000

    added detailed comments to bastion & wordpress scripts

--- modules/asg/bin/bastion.sh
+++ modules/asg/bin/bastion.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Update the package list to ensure we have the latest information on available packages
 sudo apt update -y
-sudo apt install apache2
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<h1> Hello World </h1>" 
 
+# Install the Apache2 web server
+sudo apt install apache2 -y
+
+# Start the Apache2 service to begin serving web pages
+sudo systemctl start apache2
+
+# Enable the Apache2 service to start automatically on system boot
+sudo systemctl enable apache2
+
+# Output a simple HTML message to the default web page
+echo "<h1> Hello World </h1>"
